c2af57657ec5e956f1d45fac14de9705604e1b53
Russell Salcido <russell.salcido@gmail.com>
2020-07-05 13:30:15 -0700

    updagtre to htmlroutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -15,11 +15,11 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, ""));
+    res.sendFile(path.join(__dirname, "../notes.html"));
   });
 
   // If no matching route is found default to home
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, ""));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 }; 
